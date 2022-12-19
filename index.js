06b07363183848b7b46a3c85973a875d3864ca19
HiveTrending <hive_up@protonmail.com>
2022-12-19 08:56:01 -0800

    validate player sequence (cannot reply to self)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 
+const DEBUG = process.env.DEBUG === 'true'
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -50,25 +52,37 @@ client.on("messageCreate", async (message) => {
   let outOfSequence = false;
 
   let currentNumber = -1;
+  let currentPlayer;
   for (let recentMessage of recentMessages) {
     if (currentNumber === -1) {
       currentNumber = parseInt(recentMessage[1].cleanContent);
+      currentPlayer = recentMessage[1].author.id
       continue;
     }
     const nextNumber = parseInt(recentMessage[1].cleanContent);
+    const nextPlayer = recentMessage[1].author.id
     if (currentNumber - 1 != nextNumber) {
       console.log(currentNumber, nextNumber);
       outOfSequence = true;
       console.log("number out of sequence");
     }
+    if (nextPlayer === currentPlayer) {
+      console.log(currentPlayer, nextPlayer);
+      outOfSequence = true;
+      console.log("player out of sequence");
+    }
+
     currentNumber = nextNumber;
+    currentPlayer = nextPlayer;
   }
 
   if (isNaN(parseInt(message.cleanContent)) || outOfSequence) {
     const author = message.author.username;
     console.log(`number game fail: ${message} by ${author}`);
-    announceChannel.send(`BETA: ${author} failed at number game.`);
-  } else {
+    if (!DEBUG) {
+      announceChannel.send(`BETA: ${author} failed at number game.`);
+    }
+      } else {
     console.log(`valid entry: ${message}`);
   }
 });
